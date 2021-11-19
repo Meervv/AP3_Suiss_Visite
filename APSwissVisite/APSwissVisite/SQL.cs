77311408e25d03fef4f278f475e2dfee18b140e5
YohannDR <81297939+YohannDR@users.noreply.github.com>
2021-11-19 15:27:46 +0100

    <main>(Initial Push)
    Push du projet originel

---
+++ APSwissVisite/APSwissVisite/SQL.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SqlClient;
+
+namespace APSwissVisite
+{
+    public static class SQL
+    {
+        // Propriété Data Source à changer /!\
+        private const string ConnexionString = @"Data Source=BTS2020-12\SQLEXPRESS;Initial Catalog=GSB_gesAMM;Integrated Security=True";
+        internal static SqlConnection Connexion = new SqlConnection(ConnexionString);
+    }
+}

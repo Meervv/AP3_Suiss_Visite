c90a952bfbb14a90e7607b5a54c5cb77d1278a84
Marvine Gaudrée <marvine.gaudree@stadjutor.com>
2021-12-12 11:12:27 +0100

    <hotfix>(Merge): correction bug BDD

--- APSwissVisite/APSwissVisite/SQL.cs
+++ APSwissVisite/APSwissVisite/SQL.cs
@@ -9,7 +9,7 @@ namespace APSwissVisite
     public static class SQL
     {
         // Propriété Data Source à changer /!\
-        private const string ConnexionString = @"Data Source=BTS2020-12\SQLEXPRESS;Initial Catalog=GSB_gesAMM;Integrated Security=True";
+        private const string ConnexionString = @"Data Source=BTS2020-17\SQLEXPRESS;Initial Catalog=SuissVisiteBdd;Integrated Security=True";
         internal static SqlConnection Connexion = new SqlConnection(ConnexionString);
 
         #region Fetch
